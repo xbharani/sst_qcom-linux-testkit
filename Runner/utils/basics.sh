eb2dbdf42c0b9068a4dd3e6f45e84733c05981ce
smuppand <smuppand@qti.qualcomm.com>
2025-05-12 21:46:17 +0530

    Add SPDX license headers to utility scripts
    Updates shell scripts under the utils/ directory to include proper SPDX
    license identifiers and copyright:
    
      SPDX-License-Identifier: BSD-3-Clause-Clear
    
    These changes support automation tools like repolinter and align with open
    source best practices.
    
    Signed-off-by: smuppand <smuppand@qti.qualcomm.com>

--- Runner/utils/basics.sh
+++ Runner/utils/basics.sh
@@ -1,3 +1,6 @@
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+
 # Import test suite definitions
 source ../../../../init_env
 
--- Runner/utils/platform.sh
+++ Runner/utils/platform.sh
@@ -1,3 +1,6 @@
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+
 # Detect Android userland
 ANDROID_PATH=/system/build.prop
 if [ -f $ANDROID_PATH ]; then
