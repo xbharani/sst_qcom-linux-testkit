57b662be4cd5e5690f8c7455509fd9d5d698718a
Vamsee Narapareddi <vnarapar@qti.qualcomm.com>
2025-10-13 09:07:18 +0530

    Added minor timer script fix
    Added minor fix to timer testcase to pdate the binary execution path and
    to print the complete output for easy debug
    
    Signed-off-by: Vamsee Narapareddi <vnarapar@qti.qualcomm.com>

--- Runner/suites/Kernel/Baseport/Timer/run.sh
+++ Runner/suites/Kernel/Baseport/Timer/run.sh
@@ -29,6 +29,39 @@ fi
 # shellcheck disable=SC1090,SC1091
 . "$TOOLS/functestlib.sh"
 
+# --- Parse CLI argument for custom binary path ---
+show_help() {
+    cat <<EOF
+Usage: $0 [OPTIONS]
+
+Options:
+  --binary-path=PATH   Specify a custom path to the posix_timers binary else run by default from /usr/bin.
+  -h, --help           Show this help message and exit.
+
+Example:
+  $0 --binary-path=/custom/path/posix_timers
+  $0 
+
+EOF
+}
+
+
+BINARY_PATH=""
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --binary-path=*)
+            BINARY_PATH="${1#*=}"
+            ;;
+		-h|--help)
+            show_help
+            exit 0
+            ;;
+        *)
+            ;;
+    esac
+    shift
+done
+
 TESTNAME="Timer"
 test_path=$(find_test_case_by_name "$TESTNAME")
 cd "$test_path" || exit 1
@@ -39,13 +72,24 @@ log_info "----------------------------------------------------------------------
 log_info "-------------------Starting $TESTNAME Testcase----------------------------"
 log_info "=== Test Initialization ==="
 
-chmod -R 777 /APT/timers
+# --- Determine binary to use ---
+if [ -n "$BINARY_PATH" ]; then
+    BINARY="$BINARY_PATH"
+else
+    BINARY="posix_timers"
+fi
 
-# Path to the binary
-BINARY_PATH="/var/common/bins/timers/posix_timers"
+# --- Check if binary exists in PATH or at custom path ---
+check_dependencies "$BINARY"
+if [ $? -ne 0 ]; then
+    log_skip "$TESTNAME : Binary '$BINARY' not found"
+    echo "$TESTNAME SKIP" > "$res_file"
+    exit 0
+fi
 
 # Run the binary and capture the output
-OUTPUT=$($BINARY_PATH)
+OUTPUT=$($BINARY)
+echo $OUTPUT
 
 # Check if "pass:7" is in the output
 if echo "${OUTPUT}" | grep "pass:7"; then
